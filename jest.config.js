c69fd360652ecfdbacfb4cc990330802a5e59a4d
RomainGuarinoni <romain.guar91@gmail.com>
2021-11-22 16:51:13 +0100

    feat(test): make better jest test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./test/unit/setup.ts'],
-  roots: ['test/unit/']
-};
+  roots: ['test/unit/'],
+}
--- test/unit/routes/healthcheck.spec.ts
+++ test/unit/routes/healthcheck.spec.ts
@@ -1,38 +1,45 @@
 import * as request from 'supertest'
 import app from '../../../src/server/config'
 
-import { Client } from 'pg';
+import { runQuery } from '../../../src/db/psql'
+
+jest.mock('../../../src/db/psql')
 
 describe('/healthcheck', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
 
+  const query = runQuery as jest.Mock
+
   it('returns a 200 and an OK response', async () => {
-    const client: any = new Client()
-    client.query.mockResolvedValueOnce({ rows: [{message: 'OK'}], rowCount: 1 });
+    query.mockResolvedValueOnce({
+      rows: [{ message: 'OK' }],
+      rowCount: 1,
+    })
 
     await request(app)
       .get('/healthcheck')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
+      .then((response) => {
         expect(response.body.healthstatus).toBe('OK')
       })
   })
 
   it('returns a 200 and an ERROR response', async () => {
-    const client: any = new Client()
-    client.query.mockRejectedValueOnce('boom')
+    query.mockRejectedValueOnce('boom')
 
     await request(app)
       .get('/healthcheck')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
-        expect(response.body.healthstatus).toBe('ERROR: DB healthcheck not confirmed')
+      .then((response) => {
+        expect(response.body.healthstatus).toBe(
+          'ERROR: DB healthcheck not confirmed'
+        )
       })
   })
 })
